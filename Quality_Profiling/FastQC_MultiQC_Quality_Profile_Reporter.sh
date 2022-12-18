63396760d2e2b50f3705737b011fa0bf18da15c1
kalihoras <32957466+kalihoras@users.noreply.github.com>
2022-12-18 22:35:13 +0300

    Update and rename FastQ_Quality_Profile_Reporter.sh to FastQC_MultiQC_Quality_Profile_Reporter.sh

--- Quality_Profiling/FastQC_MultiQC_Quality_Profile_Reporter.sh
+++ Quality_Profiling/FastQC_MultiQC_Quality_Profile_Reporter.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# FastQC version=v0.11.9
+# MultiQC version=1.11
 
+#cd fastq_files
 FILE_EXTENTION_RAW=fastq.gz
 FILE_LIST_RAW=$(for file in *.$FILE_EXTENTION_RAW; do echo -n "$file "; done)
 THREADS=$(cat /proc/cpuinfo | grep processor | wc -l)
